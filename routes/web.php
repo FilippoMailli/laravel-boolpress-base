596c0b9e30fb560d16000f0250deb8a9d9cb579e
Filippo Mailli <filippo.mailli@gmail.com>
2020-05-25 16:34:08 +0200

    Prime modifiche auth

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,19 @@ Route::get('/posts', 'PostController@index')->name('posts.index');
 Route::get('posts/published', 'PostController@indexPublished')->name('posts.index.published');
 
 Route::resource('posts', 'PostController');
+
+Auth::routes();
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('admin')
+->namespace('Admin')
+->name('admin.')
+->middleware('auth')
+->group(function() {
+    Route::resource('users', 'UserController');
+});
